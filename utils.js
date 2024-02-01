5aecfe7a626909e27c489d266be2cd5c2912921e
Yash Narwar <yashnarwar111@gmail.com>
2024-02-01 21:04:21 +0530

    adding movie app files

---
+++ utils.js
@@ -0,0 +1,12 @@
+// this wrapper function control the invocation time by one second of any function
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
